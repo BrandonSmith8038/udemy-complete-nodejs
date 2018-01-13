ec882943c35d8637917f5ca31fa314d99a039fa8
Brandon <brandon@reddirtwebdesign.com>
2018-01-13 09:18:42 +0000

    Using Require

---
+++ notes-node/app.js
@@ -0,0 +1,13 @@
+console.clear()
+console.log('starting app')
+
+const fs = require('fs')
+const os = require('os')
+
+const user = os.userInfo()
+
+fs.appendFile('greetings.txt',`Hello ${user.username}!`, (err) => {
+  if(err){
+    console.log('Unable to write to file')
+  }
+})
