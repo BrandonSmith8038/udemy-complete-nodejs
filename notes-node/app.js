7f4fe99c50461523c72beeb18d391c8b9897973c
Brandon <brandon@reddirtwebdesign.com>
2018-01-13 09:48:24 +0000

    Clean Up Up Example Code

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,24 +2,7 @@ console.clear()
 console.log('starting app')
 
 const fs = require('fs')
-const os = require('os')
-const notes = require('./notes.js')
 const _ = require('lodash')
 
-const res = notes.add(9,10)
-console.log('Notes.Add', res)
+const notes = require('./notes.js')
 
-console.log('Lodash', _.isString('Brandon'))
-console.log('Lodash', _.isString(true))
-
-
-let filteredArray = _.uniq(['1','2','1','3','4','3'])
-console.log('Lodash Unique', filteredArray)
-
-/*const user = os.userInfo()
-
-fs.appendFile('greetings.txt',`Hello ${user.username}! You are ${notes.age}`, (err) => {
-  if(err){
-    console.log('Unable to write to file')
-  }
-})*/
