ee7f00c45bfbe00baf8d1831de5d0026f1a9282b
Brandon <brandon@reddirtwebdesign.com>
2018-01-13 10:58:43 +0000

    Getting Input From User

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,4 +1,3 @@
-console.clear()
 console.log('starting app')
 
 const fs = require('fs')
@@ -6,3 +5,19 @@ const _ = require('lodash')
 
 const notes = require('./notes.js')
 
+console.log(process.argv)
+
+const command = process.argv[2]
+console.log('Command', command)
+
+if(command === 'add'){
+  console.log('Adding New Note')
+} else if (command === 'list'){
+  console.log('Listing All Notes')
+} else if(command === 'read'){
+  console.log('Fetching Note')
+} else if (command === 'remove'){
+  console.log('Removing Notes')
+} else {
+  console.log('Command Not Recognized')
+}
