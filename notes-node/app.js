57b2734e4fdde70dfb65145579221dde575bf89b
Brandon <brandon@reddirtwebdesign.com>
2018-01-13 09:41:52 +0000

    Requiring 3rd part modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,9 +4,17 @@ console.log('starting app')
 const fs = require('fs')
 const os = require('os')
 const notes = require('./notes.js')
+const _ = require('lodash')
 
 const res = notes.add(9,10)
 console.log('Notes.Add', res)
+
+console.log('Lodash', _.isString('Brandon'))
+console.log('Lodash', _.isString(true))
+
+
+let filteredArray = _.uniq(['1','2','1','3','4','3'])
+console.log('Lodash Unique', filteredArray)
 
 /*const user = os.userInfo()
 
