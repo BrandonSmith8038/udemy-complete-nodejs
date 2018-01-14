0422a4fdd22960d38a11b9fd0cb0d62f9cdbfb93
Brandon <brandon@reddirtwebdesign.com>
2018-01-14 10:41:31 +0000

    Fetching weather data

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,6 +1,9 @@
-const yargs = require('yargs')
+/*const yargs = require('yargs')
 
 const geocode = require('./geocode/geocode')
+const key = require('./key')
+
+const apiKey = key.keys.forcast
 
 const argv = yargs
   .options({
@@ -23,3 +26,16 @@ geocode.geocodeAdress(argv.address,(errorMessage, results) => {
   }
 })
 
+*/
+const request = require('request')
+
+request({
+  url: 'https://api.darksky.net/forecast/e04f6c49479e2bfa1c849fcd48d6d6e1/33.443341151,-112.4091489',
+  json: true
+},(error, response, body) => {
+  if(!error && response.statusCode === 200){
+    console.log(body.currently.temperature)
+  } else {
+    console.log('Unable To Fetch The Weather')
+  }
+})
