11168f3d71ea3e972cbc814dd2c37c1dc4be3ae9
Brandon Smith <brandon@reddirtwebdesign.com>
2018-01-14 10:10:15 -0700

    Switched to using promises

--- weatherApp/app-promises.js
+++ weatherApp/app-promises.js
@@ -25,12 +25,21 @@ axios
     if (response.data.status === 'ZERO_RESULTS') {
       throw new Error('Unable to find that address');
     }
-
-    const lat = '';
-    const lgn = '';
+    // console.log(response.data.results[0]);
+    const lat = response.data.results[0].geometry.location.lat;
+    const lng = response.data.results[0].geometry.location.lng;
     const weatherUrl = `https://api.darksky.net/forecast/e04f6c49479e2bfa1c849fcd48d6d6e1/${lat},${lng}`;
-    console.log(response.data);
-  })
+    console.log(response.data.results[0].formatted_address);
+    })
+    return axios.get(weatherUrl).then(response => {
+      const temperature = response.data.currently.temperature;
+      const apparentTemperature = response.data.currently.apparentTemperature;
+
+      console.log(
+        `It's currently ${temperature}. It feels like ${apparentTemperature}`
+      );
+    });
+  
   .catch(err => {
     if (err.code === 'ENOTFOUND') {
       console.log('Unable To Connect To API Service');
