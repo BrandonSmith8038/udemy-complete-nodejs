b8e65554f5fe9db4ab59d3da5ad2f2c94567639d
brandomsmith8038 <reddirtwebdesign.com>
2018-01-14 11:27:49 +0000

    Chaining Callbacks

---
+++ weatherApp/weather/weather.js
@@ -0,0 +1,23 @@
+const request = require('request')
+
+const getWeather = (lat, lng, callback) => {
+
+request({
+  url: `https://api.darksky.net/forecast/e04f6c49479e2bfa1c849fcd48d6d6e1/${lat},${lng}`,
+  json: true
+},(error, response, body) => {
+  if(!error && response.statusCode === 200){
+    callback(undefined, {
+      temperature: body.currently.temperature,
+      apparentTemperature: body.currently.apparentTemperature
+    })
+  } else {
+    callback('Unable To Get The Weather')
+  }
+})
+
+}
+
+module.exports = {
+  getWeather
+}
