cf63aa4fc6f2a8ed6c006d38e8666e8f1ca5c0d0
brandomsmith8038 <reddirtwebdesign.com>
2018-01-14 09:42:22 +0000

    Error Handling

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -20,10 +20,12 @@ const encodedAddress = argv.address
 request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true,
-}, (err, response, body) => {
-  if(err){
-    console.log(err)
-  } else {
+}, (error, response, body) => {
+  if(error){
+    console.log('Unable to connect to Google Services')
+  } else if(body.status === 'ZERO_RESULTS') {
+    console.log('Unable find that adress')
+  } else if (body.status === 'OK') {
     console.log('Address', JSON.stringify(body.results[0].formatted_address, undefined, 2))
     console.log('LATTITUDE', JSON.stringify(body.results[0].geometry.location.lat, undefined, 2))
     console.log('LONGTITUDE', JSON.stringify(body.results[0].geometry.location.lng, undefined, 2))
