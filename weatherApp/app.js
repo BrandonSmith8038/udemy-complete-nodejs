31301b8c166b76b2534db5917b7c669d98d005e1
Brandon <brandon@reddirtwebdesign.com>
2018-01-14 09:59:05 +0000

    Abstracting Callbacks

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,6 +1,6 @@
-const request = require('request')
 const yargs = require('yargs')
 
+const geocode = require('./geocode/geocode')
 
 const argv = yargs
   .options({
@@ -15,19 +15,11 @@ const argv = yargs
 .alias('help', 'h')
 .argv
 
-const encodedAddress = argv.address
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true,
-}, (error, response, body) => {
-  if(error){
-    console.log('Unable to connect to Google Services')
-  } else if(body.status === 'ZERO_RESULTS') {
-    console.log('Unable find that adress')
-  } else if (body.status === 'OK') {
-    console.log('Address', JSON.stringify(body.results[0].formatted_address, undefined, 2))
-    console.log('LATTITUDE', JSON.stringify(body.results[0].geometry.location.lat, undefined, 2))
-    console.log('LONGTITUDE', JSON.stringify(body.results[0].geometry.location.lng, undefined, 2))
+geocode.geocodeAdress(argv.address,(errorMessage, results) => {
+  if(errorMessage){
+  console.log(errorMessage)
+  } else {
+  console.log(JSON.stringify(results, undefined, 2))
   }
 })
+
