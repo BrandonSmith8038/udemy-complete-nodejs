a1ecfa39d4a28563cb68a7199a5f7da4fbae9ed5
brandomsmith8038 <reddirtwebdesign.com>
2018-01-14 13:11:49 +0000

    Promises practice

---
+++ playground/promises.js
@@ -0,0 +1,32 @@
+const asyncAdd = (a,b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(typeof a === 'number' && typeof b === 'number'){
+        resolve(a + b)
+      } else {
+        reject('Arguments Must Numbers')
+      }
+    }, 1500)
+  })
+}
+
+/*const somePromise =  new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve('Hey, It worked')
+    reject('Unable to fullfill promise')
+  }, 2500)
+  
+})
+
+somePromise.then((message) => {
+  console.log('Success:', message)
+}, (err) => {
+  console.log('Err: ', err)
+})*/
+
+asyncAdd(5,7).then(result => {
+  console.log('Result: ', result)
+  return asyncAdd(result, 33)
+}).then(res => {
+  console.log('Should Be 45: ', res)
+}).catch(err =>  console.log(err))
---
+++ playground/promises2.js
@@ -0,0 +1,26 @@
+const request = require('request')
+
+const geocodeAddress = (address) => {
+  return new Promise((resolve, reject) => {
+    request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
+    json: true,
+  }, (error, response, body) => {
+    if(error){
+      reject('Unable to connect to Google Services')
+    } else if(body.status === 'ZERO_RESULTS') {
+      reject('Unable find that adress')
+    } else if (body.status === 'OK') {
+      resolve({
+        address: body.results[0].formatted_address,
+        lattitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng
+      })
+    }
+  })
+  })
+}
+
+geocodeAddress('16277 W. Jackson Goodyear AZ')
+  .then(result => console.log(result))
+  .catch(err => console.log(err))
