775ee5fdddb12fa6c83bc6985d9994417e7c14b7
Brandon <brandon@reddirtwebdesign.com>
2018-01-18 12:16:55 +0000

    Using assertion libaries

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,18 +1,44 @@
+const expect = require('expect')
+
 const utils = require('./utils')
+
 
 it('Should add two numbers', () => {
   
   const res = utils.add(33,11)
   
-  if(res !== 44){
-    throw new Error(`Expected 44, but got ${res}`)
-  }
+  expect(res).toBe(44).toBeA('number')
 })
 
 it('Should square a number', () => {
   const res = utils.square(8)
   
-  if(res !== 64){
-    throw new Error(`Expected 64 but got ${res}`)
+  expect(res).toBe(64).toBeA('number')
+})
+
+/*it('Should expect some values', () => {
+  // expect(12).toNotBe(11)
+  // expect({name: 'brandon'}).toNotEqual({name: 'Brandon'})
+  // expect([2,3,4]).toInclude(3)
+  // expect([2,3,4]).toNotInclude(1)
+  expect({
+    name: 'Brandon',
+    age: 32,
+    location: 'Scottsdale'
+  }).toInclude({
+    age: 32
+  })
+})*/
+
+//Should verify first and last names are set
+it('Should set first and last name', () => {
+  const user = {
+   age: 32,
+   location: 'Scottsdale'
   }
+  
+  const res = utils.setName(user, 'Brandon Smith')
+  
+  expect(res).toInclude({firstName: 'Brandon'}).toBeA('object')
+  expect(res).toInclude({lastName: 'Smith'}).toBeA('object')
 })
