5e5110ab2dc7c45291c6e108f471b91439b96d67
Brandon <brandon@reddirtwebdesign.com>
2018-01-18 10:47:04 +0000

    Express Middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const hbs = require('hbs')
+const fs = require('fs')
 
 const app = express();
 
 app.set('view engine', 'hbs')
 hbs.registerPartials(__dirname + '/views/partials')
-app.use(express.static(__dirname + '/public'));
+
+
+app.use((req, res, next) => {
+  const now = new Date().toString()
+  
+  const log = (`${now}: ${req.method} ${req.url}`)
+  
+  fs.appendFile('server.log', log + '\n', err => {
+    if(err) {
+      console.log(`Unable to append to the log file`)
+    }
+  })
+  console.log(log)
+  next()
+})
+
+/*app.use((req, res, next) => {
+  res.render('maintence')
+})*/
 
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear())
 hbs.registerHelper('screamIt', text => text.toUpperCase())
 
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express</h1>');
