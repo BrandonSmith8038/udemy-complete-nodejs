48b16fa5d0b1ad7e7956d0417ee0da299466fe19
Brandon <brandon@reddirtwebdesign.com>
2018-01-18 09:23:17 +0000

    Rendering Templates With Dynamic Data

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,19 +1,31 @@
 const express = require('express');
+const hbs = require('hbs')
 
 const app = express();
 
+app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'));
+
+
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express</h1>');
-  res.send({
+  /*res.send({
     name: 'Brandon',
     likes: ['Coding', 'Technolgy', 'Rodeo']
-  });
+  });*/
+  res.render('home', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome to my awesome express site'
+  })
 });
 
 app.get('/about', (req, res) => {
-  res.send('This is the about page');
+  res.render('about', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  })
 });
 
 app.get('/bad', (req, res) => {
