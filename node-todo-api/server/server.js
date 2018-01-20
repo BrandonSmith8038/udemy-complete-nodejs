bbbe0c6a83a8583db249ce0ecdeb93ef8a4d779a
Brandon <brandon@reddirtwebdesign.com>
2018-01-20 11:42:50 +0000

    Generating Tokens and Headers

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -100,16 +100,17 @@ app.patch('/todos/:id', (req, res) => {
 })
 
 app.post('/users', (req, res) => {
-  const body = _.pick(req.body,['email', 'password'])
-  
-  const user = new User(body)
-  
-  user.save().then(doc => {
-    res.send(doc)
-  }).catch(e => {
-    res.status(400).send(e)
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
   })
-})
+});
   
 
 const port = process.env.PORT || 3000
