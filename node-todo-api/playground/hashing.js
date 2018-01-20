0c69fc93d91a22be4a046cb8cedef7c535232426
Brandon <brandon@reddirtwebdesign.com>
2018-01-20 13:17:22 +0000

    Hashing Passwords

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,7 +1,22 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-const data = {
+const password = '123abc!'
+
+/*bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash)
+  })
+})*/
+
+const hashedPassword = '$2a$10$WHRPECJFQA.JLYu9i0khnebEea73OyKmxj26cvKzUoPyhGXA64cSi'
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+  console.log(result)
+})
+
+/*const data = {
   id: 10
 }
 
@@ -12,7 +27,7 @@ console.log(token)
 
 
 const decoded = jwt.verify(token, '123abc')
-console.log('Decoded: ', decoded)
+console.log('Decoded: ', decoded)*/
 
 
 /*const message = 'I am user number 3'
