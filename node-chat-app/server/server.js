0c22cf2a7ff4a5775cab99a181ddd311d7505ae4
Brandon <brandon@reddirtwebdesign.com>
2018-01-21 13:21:34 +0000

    Adding Socket.io

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
 const path = require('path')
 
 const publicPath = path.join(__dirname, '../public')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
 
 app.use(express.static(publicPath))
+
+
+
+io.on('connection', socket => {
+  console.log('New User Connected')
+  
+  socket.on('disconnect', () => {
+    console.log('User Disconnected')
+  })
+})
 
 
 
@@ -14,6 +28,6 @@ app.use(express.static(publicPath))
 const port = process.env.PORT || 3000
 const ip = process.env.IP
 
-app.listen(port, ip, () => {
+server.listen(port, ip, () => {
   console.log(`App started on port ${port}`)
 })
