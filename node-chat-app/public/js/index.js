0390bc3bf08d5ae45e3f31f3818fae8809e920ed
Brandon <brandon@reddirtwebdesign.com>
2018-01-25 11:40:10 +0000

    Add form for new message and show incoming message in broswer

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -11,4 +11,32 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', message => {
   console.log('New Message', message)
+  const messages = document.getElementById('messages')
+  const li = document.createElement('li')
+    
+  li.className = 'list-group-item'
+  li.appendChild(document.createTextNode(`${message.from}: ${message.text}`))
+  
+    
+  messages.appendChild(li)  
+    
+})
+
+const messageForm = document.getElementById('message-form');
+
+messageForm.addEventListener('submit', (e) => {
+  
+  const message = document.getElementById('message')
+  
+  e.preventDefault()
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: message.value
+  }, () => {
+    
+  })
+  
+    message.value = ''
+  
 })
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -21,9 +21,10 @@ io.on('connection', socket => {
   
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Has Joined'))
   
-  socket.on('createMessage', message => {
+  socket.on('createMessage', (message, callback) => {
     console.log('New Message', message)
     io.emit('newMessage', generateMessage(message.from, message.text))
+    callback('This is from the server')
     
     /*socket.broadcast.emit('createMessage', {
       from: message.from,
